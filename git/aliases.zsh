009ee7af047fcb54490682cf7810a76b40bf8789
Blake Walters <blake@markupboy.com>
2011-05-05 17:26:56 -0600

    updated git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,14 +15,17 @@ alias gdiff='git difftool'
 alias add='git add'
 
 #shorter aliases
+alias gb='git branch'
 alias gmv='git mv'
 alias gr='git rebase'
 alias gc='git commit'
 alias gco='git checkout'
 alias gca='git commit -a'
 alias gp='git push origin HEAD'
-alias gl='git pull'
+alias gl='git pull --prune'
 alias gm='git merge'
 alias gs='git status -sb'
 alias gd='git difftool'
 alias ga='git add'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
