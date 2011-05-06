ad0a1cf011cee987cfd524b468613189185f5ea3
Blake Walters <blake@markupboy.com>
2011-05-05 22:29:54 -0600

    added django aliases

---
+++ django/aliases.zsh
@@ -0,0 +1,5 @@
+#django
+alias runserver='python manage.py runserver'
+alias syncdb='pyhton manage.py syncdb'
+alias migrate='python manage.py migrate'
+alias schemamigration='python manage.py schemamigration'
