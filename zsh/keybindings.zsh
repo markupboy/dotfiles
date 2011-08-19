99b5894e3ee5e7a838d275fdf73ae56ec41e55a0
Blake Walters <blake@markupboy.com>
2011-08-19 16:06:18 -0600

    removed back and forward word keybindings (fixed directly in iTerm)

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -10,8 +10,8 @@ bindkey "^r" history-incremental-search-backward
 bindkey ' ' magic-space			# also do history expansion on space
 bindkey '^I' complete-word	# complete on tab, leave expansion to _expand
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
+# bindkey '^[^[[D' backward-word
+# bindkey '^[^[[C' forward-word
 # bindkey '^[[5D' beginning-of-line
 # bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
