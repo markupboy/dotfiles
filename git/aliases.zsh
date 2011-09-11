6a4a22a304a3802b177f2a089c6a57babbf8e085
Blake Walters <blake@markupboy.com>
2011-09-10 20:41:26 -0600

    added aliases for git undo functions

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -21,3 +21,5 @@ alias ga='git add'
 alias gaa='git-all'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gu="git undo"
+alias gpu="git push -f origin HEAD^:master"
