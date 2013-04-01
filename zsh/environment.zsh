b1abae2d95e121add42bf12281537fc3b093f43f
Blake Walters <blake@markupboy.com>
2013-04-01 10:48:33 -0600

    moved some stuff around

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -8,6 +8,8 @@ else
     export TERM='xterm-color'
 fi
 
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/git/libexec/git-core:$PATH" 
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/git/libexec/git-core:$PATH"
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
 export EDITOR='mate -w'
+export EDITOR="$DOTFILES/bin/editor"
+export PATH="/usr/local/heroku/bin:$PATH"
