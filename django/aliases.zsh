73a8aa3601542df303fdb5496818dc2ac088f6db
Blake Walters <blake@markupboy.com>
2015-02-25 14:26:36 -0700

    removed django

--- django/aliases.zsh
+++
@@ -1,9 +0,0 @@
-#django
-alias runserver='python manage.py runserver'
-alias ds='runserver'
-alias syncdb='pyhton manage.py syncdb'
-alias dsdb='syncdb'
-alias migrate='python manage.py migrate'
-alias dm='migrate'
-alias schemamigration='python manage.py schemamigration'
-alias dsm='schemamigration'
--- django/functions.zsh
+++
@@ -1,8 +0,0 @@
-startproject () {
-	mkproject $1;
-	reload\!;
-	pip install Django;
-	django-admin.py startproject --template==$TEMPLATE_PATH $1;
-	cd $1;
-	pip install -r requirements.txt;
-}
