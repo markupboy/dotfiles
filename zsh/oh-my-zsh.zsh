57eabcd297d481148d95b0f8f3c8d986dba27ef6
Blake Walters <blake@markupboy.com>
2017-03-07 20:16:55 -0700

    update oh-my-zsh theme to agnoster

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -5,7 +5,8 @@ ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="markupboy"
+# ZSH_THEME="markupboy"
+ZSH_THEME="agnoster"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
@@ -29,7 +30,7 @@ DISABLE_AUTO_UPDATE="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(rbenv rails rails3 brew bundler cap gem git github osx rake)
+plugins=(rbenv rails brew bundler cap gem github osx rake zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
