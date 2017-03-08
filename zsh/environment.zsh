5dbb2b0ce3400c4d3717f616a4d34687d5978a4e
Blake Walters <blake@markupboy.com>
2017-03-07 20:16:21 -0700

    removed extra path information from environment

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -7,8 +7,3 @@ if [ -e /usr/share/terminfo/x/xterm-256color ]; then
 else
   export TERM='xterm-color'
 fi
-
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/git/libexec/git-core:$PATH"
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
-export PATH="/usr/local/share/npm/bin:$PATH"
-export PATH="/usr/local/heroku/bin:$PATH"
