0650fc4eab44f772ffb3f5c7a60a9fcead924a31
Blake Walters <blake@markupboy.com>
2023-03-02 08:14:53 -0700

    automate standalone ohmyzsh install

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -2,29 +2,8 @@
 ZSH=$HOME/.oh-my-zsh
 
 # spaceship theme and settings (https://github.com/denysdovhan/spaceship-prompt/blob/master/docs/Options.md)
+export SPACESHIP_CONFIG="$HOME/.spaceshiprc.zsh"
 ZSH_THEME="spaceship"
-SPACESHIP_PROMPT_ORDER=(
-  time          # Time stamps section
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git
-  node
-  golang
-  docker
-  exec_time     # Execution time
-  line_sep      # Line break
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
-)
-SPACESHIP_CHAR_SUFFIX=" "
-SPACESHIP_CHAR_SYMBOL="›"
-SPACESHIP_DIR_TRUNC_REPO=false
-SPACESHIP_GIT_STATUS_ADDED="%F{yellow}+%F{red}"
-SPACESHIP_GIT_STATUS_UNTRACKED="%F{blue}?%F{red}"
-SPACESHIP_GIT_STATUS_DELETED="%F{green}x%F{red}"
-SPACESHIP_GIT_STATUS_MODIFIED="%F{red}!%F{red}"
 
 # Comment this out to disable weekly auto-update checks
 DISABLE_AUTO_UPDATE="true"
