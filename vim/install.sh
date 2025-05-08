506b614eefabbf32a420efbdcdc720d361d125a9
Blake Walters <blake@markupboy.com>
2025-05-08 11:01:56 -0600

    temporarily disable vim install

--- vim/install.sh
+++ vim/install.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
-mkdir -p ~/.vim/colors
-curl -o ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/colors/onedark.vim
+# mkdir -p ~/.vim/colors
+# curl -o ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/colors/onedark.vim
 
-mkdir -p ~/.vim/autoload
-curl -o ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/onedark.vim
+# mkdir -p ~/.vim/autoload
+# curl -o ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/onedark.vim
 
-mkdir -p ~/.vim/autoload/airline/themes
-curl -o ~/.vim/autoload/airline/themes/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/airline/themes/onedark.vim
+# mkdir -p ~/.vim/autoload/airline/themes
+# curl -o ~/.vim/autoload/airline/themes/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/airline/themes/onedark.vim
 
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-fi
+# if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+#   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#   vim +PluginInstall +qall
+# fi
