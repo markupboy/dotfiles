5bd3515226d85d7af638a86c6eb1a622ebb23577
Blake Walters <blake@markupboy.com>
2025-05-08 10:28:03 -0600

    install wget if not available

--- vim/install.sh
+++ vim/install.sh
@@ -4,10 +4,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 mkdir -p ~/.vim/colors
-wget --output-document ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/colors/onedark.vim
+curl -o ~/.vim/colors/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/colors/onedark.vim
 
 mkdir -p ~/.vim/autoload
-wget --output-document ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/onedark.vim
+curl -o ~/.vim/autoload/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/onedark.vim
 
 mkdir -p ~/.vim/autoload/airline/themes
-wget --output-document ~/.vim/autoload/airline/themes/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/airline/themes/onedark.vim
+curl -o ~/.vim/autoload/airline/themes/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.vim/refs/heads/main/autoload/airline/themes/onedark.vim
