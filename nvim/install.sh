f66a03b9aba75526ee0133a271e7815e84ae50da
Blake Walters <blake@markupboy.com>
2025-05-09 17:55:38 -0600

    fixed symlinking of config dirs

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config
+if [ ! -d "$HOME/.config/nvim" ]; then
+  mkdir -p $HOME/.config
 
-# Get the directory of the script
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  # Get the directory of the script
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Create or overwrite the symbolic link
-ln -sf "$script_dir" "$HOME/.config/nvim"
+  # Create or overwrite the symbolic link
+  ln -s "$script_dir" "$HOME/.config/nvim"
+fi
