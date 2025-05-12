79f79939da6bc51fa5d19cbdace1e392c148bddf
Blake Walters <blake@markupboy.com>
2025-05-11 20:17:25 -0600

    so long, ruby

--- ruby/aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias rake='noglob rake'
-alias bundle='noglob bundle'
--- ruby/rbenv.zsh
+++
@@ -1,6 +0,0 @@
-if [[ -a ~/.rbenv ]]
-then
-  export PATH=~/.rbenv/shims:$PATH
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(~/.rbenv/bin/rbenv init - zsh)"
-fi
