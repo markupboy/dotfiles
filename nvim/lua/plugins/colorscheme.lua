cca0f42a95adde054954fc7f512441a82a8707a2
Blake Walters <blake@markupboy.com>
2025-05-21 20:49:43 -0600

    update nvim settings

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,8 +6,6 @@ return {
       vim.o.termguicolors = true
       vim.cmd.colorscheme("onedark")
 
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
   },
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,33 +1,27 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.3", -- Use the latest stable version
-    dependencies = { "nvim-lua/plenary.nvim" }, -- Required dependency
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-          },
-          prompt_prefix = "> ",
-          selection_caret = "> ",
-          entry_prefix = "  ",
-          sorting_strategy = "ascending",
-          layout_config = {
-            horizontal = { prompt_position = "top", results_width = 0.6 },
-            vertical = { mirror = false },
-          },
-          file_ignore_patterns = { "node_modules", ".git" },
-          path_display = { "truncate" },
-          winblend = 0,
-        },
-      })
-    end,
-  },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  module = "telescope",
+
+  config = function()
+    require('telescope').setup({})
+
+    local builtin = require('telescope.builtin')
+
+    vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>fr", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", ":Telescope find_files hidden=true <CR>")
+
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word })
+    end)
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({ search = word })
+    end)
+  end
 }
