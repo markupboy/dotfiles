175343b22eb84edcc2c22ec00231e6679d20bf7f
Blake Walters <blake@markupboy.com>
2025-05-23 18:30:57 -0600

    moved python install to sh so it doesn't automatically get execution

--- python/install.sh
+++ python/install.sh
@@ -1,7 +1,8 @@
-if (( $+commands[uv] ))
+if command -v uv >/dev/null 2>&1
 then
     if [[ ! -f ~/.zsh/completions/_uv ]]
     then
+        mkdir -p ~/.zsh/completions
         uv generate-shell-completion zsh > ~/.zsh/completions/_uv
     fi
 fi
