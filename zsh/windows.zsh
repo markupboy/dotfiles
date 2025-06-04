366ebd602efa2f6800de6b04bb93ac4cb26c6397
Blake Walters <blake@markupboy.com>
2025-06-04 11:05:43 -0600

    alias ssh for windows

--- zsh/windows.zsh
+++ zsh/windows.zsh
@@ -5,6 +5,8 @@ then
     export PATH="$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin"
     alias docker=docker.exe
     alias docker-compose=docker-compose.exe
+    alias ssh='ssh.exe'
+    alias ssh-add='ssh-add.exe'
 fi
 
 if [[ -a /home/linuxbrew/.linuxbrew/bin ]]
