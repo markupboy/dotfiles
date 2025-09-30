250cf841eb348c3b343d1736d186bd5d1047ea49
Blake Walters <blake@markupboy.com>
2025-09-30 08:11:16 -0600

    fixed node loading via nvm

--- node/node.zsh
+++ node/node.zsh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh" --no-use
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # pnpm
