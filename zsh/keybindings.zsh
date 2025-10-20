584df8e79476c786fb0473a898e30019ce18f763
Blake Walters <blake@markupboy.com>
2025-10-20 07:58:57 -0600

    fixed home and end keys

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,6 +1,10 @@
 # keybindings
 bindkey '^[OH' beginning-of-line
+bindkey '\e[H' beginning-of-line
+bindkey '\e[1~' beginning-of-line
 bindkey '^[OF' end-of-line
+bindkey '\e[F' end-of-line
+bindkey '\e[4~' end-of-line
 bindkey '^[[5~' up-line-or-history
 bindkey '^[[6~' down-line-or-history
 bindkey "^r" history-incremental-search-backward
