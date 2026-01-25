997624b2c2cdc795fdced09e1f30a5111585bccd
markupboy <blake@markupboy.com>
2026-01-25 11:11:41 -0700

    added linux install script for starship

---
+++ starship/install.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+if [[ $LINUX ]]; then
+  if [ ! -f "/usr/local/bin/starship" ]; then
+    dotlog "install" "Installing starship prompt"
+    curl -sS https://starship.rs/install.sh | sh
+  else
+    dotlog "skip" "Skipping starship prompt"
+  fi
+fi
