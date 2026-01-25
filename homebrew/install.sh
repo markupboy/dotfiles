fec8fcb539734cd40177ff3e8352a9f8c7ebbc21
markupboy <blake@markupboy.com>
2026-01-24 19:12:05 -0700

    made things a little bit more linux friendly

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,18 +5,24 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if command -v brew >/dev/null 2>&1; 
+if ! [[ $WIN || $LINUX ]]; 
 then
-  dotlog "skip" "Homebrew already installed"
-else
-  dotlog "install" "Installing Homebrew for you"
 
-  # Install the correct homebrew for Mac or Linux
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  
-  dotlog "info" "Eval'ing brew shellenv"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  # Check for Homebrew
+  if command -v brew >/dev/null 2>&1; 
+  then
+    dotlog "skip" "Homebrew already installed"
+  else
+    dotlog "install" "Installing Homebrew for you"
+
+    # Install the correct homebrew for Mac or Linux
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    
+    dotlog "info" "Eval'ing brew shellenv"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+else
+  dotlog "skip" "Skipping homebrew"
 fi
 
 exit 0
