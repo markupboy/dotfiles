626153d58450d6c856ac91f10ecdbb746c8536d2
Blake Walters <blake@markupboy.com>
2026-01-25 12:01:51 -0700

    updated macos keyboarding defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
 # Use AirDrop over every interface
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
