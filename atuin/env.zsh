476ca8bdde961d05253796979310ef199fb14574
Blake Walters <blake@markupboy.com>
2026-01-27 08:23:56 -0700

    have atuin only bound to CTRL+R and not the up arrow

--- atuin/env.zsh
+++ atuin/env.zsh
@@ -1,6 +1,6 @@
 if [[ -a ~/.atuin/bin/env ]]; then
   . "$HOME/.atuin/bin/env"
-  eval "$(atuin init zsh)"
+  eval "$(atuin init zsh --disable-up-arrow)"
 fi
 
 export ATUIN_CONFIG_DIR=$DOTFILES/atuin
