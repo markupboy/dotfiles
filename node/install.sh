64d374de19df90cef5c08cb20860c48ba51a15e8
Blake Walters <blake@markupboy.com>
2026-03-20 09:07:03 -0600

    switch from nvm to fnm

---
+++ node/install.sh
@@ -0,0 +1,3 @@
+if [ ! -d "$HOME/.local/share/fnm" ]; then
+  curl -fsSL https://fnm.vercel.app/install | bash
+fi
--- node/node.zsh
+++ node/node.zsh
@@ -1,6 +1,14 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# fnm
+export PATH="/Users/blake/.local/state/fnm_multishells/43474_1774018937226/bin":$PATH
+export FNM_MULTISHELL_PATH="/Users/blake/.local/state/fnm_multishells/43474_1774018937226"
+export FNM_VERSION_FILE_STRATEGY="local"
+export FNM_DIR="/Users/blake/.local/share/fnm"
+export FNM_LOGLEVEL="info"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+export FNM_COREPACK_ENABLED="false"
+export FNM_RESOLVE_ENGINES="true"
+export FNM_ARCH="arm64"
+rehash
 
 # pnpm
 export PNPM_HOME="/Users/blake/Library/pnpm"
@@ -8,4 +16,3 @@ case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
-# pnpm end
