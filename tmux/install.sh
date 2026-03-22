5706efb80831a0e1d469e9e9652a72518e01538a
Blake Walters <blake@markupboy.com>
2026-03-22 13:00:51 -0600

    added tmux install script

---
+++ tmux/install.sh
@@ -0,0 +1,8 @@
+if [ ! -d "$HOME/.tmux/plugins" ]; then
+  dotlog "install" "Setting up tmux"
+  
+  mkdir -p $HOME/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  dotlog "skip" "Skipping tmux"
+fi
