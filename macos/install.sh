57701605f7775eba8695670b7b54ba715af79db1
Blake Walters <blake@markupboy.com>
2026-03-23 09:17:49 -0600

    I never actually want to run softwareupdate via the commandline

--- macos/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-set -e
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  dotlog "info" "Running MacOS software update"
-  sudo softwareupdate -i -a
-else
-  dotlog "skip" "Skipping - not a mac"
-fi
