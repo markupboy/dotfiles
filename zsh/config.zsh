645dd6f65ca0683ae7df93c7fe75a61054974860
Blake Walters <blake@markupboy.com>
2026-03-23 19:27:24 -0600

    combined zsh correction, completion, into main config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -3,6 +3,7 @@ fpath=($ZSH/zsh/functions $fpath)
 
 autoload -U $ZSH/zsh/functions/*(:t)
 
+setopt HIST_NO_STORE # don't store 'history' command in the history
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
@@ -14,8 +15,36 @@ setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+setopt EXTENDED_GLOB
+
+# completion settings
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # matches case insensitive for lowercase
+zstyle ':completion:*' insert-tab pending # pasting with tabs doesn't perform completion
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
+zstyle ':completion:*' menu select=1 _complete _ignored _approximate
+zstyle -e ':completion:*:approximate:*' max-errors \
+    'reply=( $(( ($#PREFIX+$#SUFFIX)/2 )) numeric )'
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
+zstyle ':completion:*:expand:*' tag-order all-expansions
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format ' -%B%d%b- '
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:processes-names' command 'ps axho command'
+if [[ "$(uname)" == "Darwin" ]]; then
+  zstyle ':completion:*:*:kill:*:processes' command 'ps -A -o pid,user,comm'
+else
+  zstyle ':completion:*:*:kill:*:processes' command 'ps --forest -A -o pid,user,cmd'
+fi
