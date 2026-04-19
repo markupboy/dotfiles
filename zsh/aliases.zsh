d5dff8dd71224530bd4a8bfbf8aa9f6c39f10f87
Blake Walters <blake@markupboy.com>
2026-04-19 08:31:37 -0600

    fixed dotvim alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 #zsh/dotfile specific
 alias dotcode='code $DOTFILES'
 alias dotzed='zed $DOTFILES'
-alias dotvim='vim $DOTFILES'
+alias dotvim='cd $DOTFILES && vim .'
 alias dotcd='cd $DOTFILES'
 
 alias reload!='. ~/.zshrc && clear'
