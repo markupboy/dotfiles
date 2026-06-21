33d8c87d994c8c881f3a9f9c07be6efab36cc0fa
Blake Walters <blake@markupboy.com>
2026-06-20 19:06:44 -0600

    fix zoxide for claude

--- zoxide/aliases.zsh
+++
@@ -1,4 +0,0 @@
-if command -v zoxide &>/dev/null
-then
-  alias cd='z'
-fi
--- zoxide/zoxide.zsh
+++ zoxide/zoxide.zsh
@@ -1,5 +1,6 @@
 if command -v zoxide &>/dev/null; then
   if [[ -z "${CLAUDECODE}" ]]; then
     eval "$(zoxide init zsh)"
+    alias cd='z'
   fi
 fi
