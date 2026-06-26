5994bb91740c41c2aeb2be99941332d749de0394
Blake Walters <blake@markupboy.com>
2026-06-26 09:34:19 -0600

    switched to worktrunk

--- wt/install.sh
+++ wt/install.sh
@@ -1,17 +1,17 @@
 #!/bin/zsh
 set -e
 
-if [ ! -d "$HOME/.config/wt" ]; then
-  dotlog "install" "Installing wt"
+if [ ! -d "$HOME/.config/worktrunk" ]; then
+  dotlog "install" "Installing worktrunk"
   mkdir -p "$HOME/.config"
 
   # Get the directory of the script
   script_dir=$(dirname "$(realpath "$0")")
 
   # Create or overwrite the symbolic link
-  ln -s "$script_dir" "$HOME/.config/wt"
+  ln -s "$script_dir" "$HOME/.config/worktrunk"
 
-  dotlog "info" "Be sure to install wt for your platform"
+  dotlog "info" "Be sure to install worktrunk for your platform"
 else
-  dotlog "skip" "Skipping wt"
+  dotlog "skip" "Skipping worktrunk"
 fi
