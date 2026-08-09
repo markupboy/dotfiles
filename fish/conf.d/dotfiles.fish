b571187fdff5cf11b1fdf9f121ff0bed8993bb80
Blake Walters <blake@markupboy.com>
2026-08-09 08:13:32 -0600

    go back to the properly banged reload\!

--- fish/conf.d/dotfiles.fish
+++ fish/conf.d/dotfiles.fish
@@ -3,7 +3,7 @@ if status is-interactive
     alias dotvim 'cd $DOTFILES; and vim .'
     alias dotcd 'cd $DOTFILES'
 
-    function reload --description "Reload fish config and clear the screen"
+    function reload! --description "Reload fish config and clear the screen"
       source ~/.config/fish/config.fish
       clear
     end
